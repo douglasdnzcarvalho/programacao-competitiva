928d9d516d906f82356d01df9da897405b253ae6
Douglas Diniz Carvalho <douglasdnzcarvalho@gmail.com>
2020-08-05 23:23:01 -0300

    Finalização do testrunner

--- testrunner.spec.js
+++ testrunner.spec.js
@@ -1,16 +1,83 @@
-const { readdirSync } = require('fs');
+const { spawn } = require('child_process');
+const { readdirSync, readFileSync } = require('fs');
 
-let foldersList = readdirSync('.', { withFileTypes: true })
-    .filter(file => file.isDirectory() && /^[0-9]+$/.test(file.name))
-    .map(folder => folder.name);
-
-describe.each(foldersList)('Problem %s', testProblem);
+describe.each(getProblemsFolders('.'))('Problem %s', testProblem);
 
 function testProblem(problemNumber) {
-    test('first function', () => {
-        expect(problemNumber).toBe('1001');
+    let folderPath = './' + problemNumber;
+
+    test.each(getValidTestCases(folderPath))('Scenario %s', async (scenarioNumber) => {
+        let expected = readFileSync(`${folderPath}/output_${scenarioNumber}.txt`, 'UTF-8');
+        let received = await executeScript(folderPath, scenarioNumber);
+
+        expect(received).toBe(expected);
+    })
+}
+
+function getProblemsFolders(path) {
+    return readdirSync(path, { withFileTypes: true })
+        .filter(file => file.isDirectory() && /^[0-9]+$/.test(file.name))
+        .map(folder => folder.name);
+}
+
+function getValidTestCases(path) {
+    return readdirSync(path, { withFileTypes: true })
+        .filter(file => !file.isDirectory() && /^(input|output)_[0-9]+.txt$/.test(file.name))
+        .reduce((allTests, file) => {
+            let number = file.name.match(/[0-9]+/g)[0];
+            let type = /^input_/.test(file.name) ? 'input' : 'output';
+            let testCase = allTests.find(c => c.number == number);
+
+            if (testCase == undefined) {
+                testCase = { number: number }
+
+                if (type == 'input') {
+                    testCase.input = true;
+                } else {
+                    testCase.output = true;
+                }
+
+                allTests.push(testCase);
+            } else {
+                if (type == 'input') {
+                    testCase.input = true;
+                } else {
+                    testCase.output = true;
+                }
+            }
+
+            return allTests;
+        }, [])
+        .filter(testCase => testCase.input && testCase.output)
+        .map(testCase => testCase.number);
+}
+
+async function executeScript(path, scenarioNumber) {
+    const script = spawn('node', [`${path}/index.js`]);
+    
+    const input = readFileSync(`${path}/input_${scenarioNumber}.txt`, 'UTF-8');
+
+    script.stdin.write(input);
+    script.stdin.end();
+
+    let output = "";
+    let error = "";
+
+    for await (const chunk of script.stdout) {
+        output += chunk.toString();
+    }
+
+    for await (const chunk of script.stderr) {
+        error += chunk;
+    }
+
+    const exitCode = await new Promise((resolve, reject) => {
+        script.on('close', resolve);
     });
-    test('second function', () => {
-        expect(problemNumber).toBe('1002');
-    });
+
+    if (exitCode) {
+        throw new Error(`Script failed with code ${exitCode}: ${error}`);
+    }
+
+    return output;
 }
