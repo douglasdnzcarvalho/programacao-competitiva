8f0eff45582934aab9fd236ebfca84658d9f4092
Douglas Diniz Carvalho <douglasdnzcarvalho@gmail.com>
2020-08-09 00:58:57 -0300

    Reformulado o testrunner para executar testes que possuem apenas output.

--- testrunner.spec.js
+++ testrunner.spec.js
@@ -1,23 +1,31 @@
 const { spawn } = require('child_process');
 const { readdirSync, readFileSync } = require('fs');
 
-describe.each(getProblemsFolders('.'))('Problem %s', testProblem);
+describe.each(getTestSuites('.'))('Problem %s', testProblem);
 
-function testProblem(problemNumber) {
+function testProblem(problemNumber, cases) {
     let folderPath = './' + problemNumber;
 
-    test.each(getValidTestCases(folderPath))('Scenario %s', async (scenarioNumber) => {
-        let received = await executeScript(folderPath, scenarioNumber);
+    test.each(cases)('Scenario %s', async (scenarioNumber, testCase) => {
+        let received = await executeScript(folderPath, scenarioNumber, testCase);
         let expected = readFileSync(`${folderPath}/output_${scenarioNumber}.txt`, 'UTF-8');
 
         expect(received).toBe(expected);
     })
 }
 
-function getProblemsFolders(path) {
+function getTestSuites(path) {
     return readdirSync(path, { withFileTypes: true })
         .filter(file => file.isDirectory() && /^[0-9]+$/.test(file.name))
-        .map(folder => folder.name);
+        .reduce((suites, folder) => {
+            let testSuite = getValidTestCases('./' + folder.name);
+
+            if (testSuite.length) {
+                suites.push([folder.name, testSuite])
+            }
+
+            return suites;
+        }, []);
 }
 
 function getValidTestCases(path) {
@@ -26,39 +34,32 @@ function getValidTestCases(path) {
         .reduce((allTests, file) => {
             let number = file.name.match(/[0-9]+/g)[0];
             let type = /^input_/.test(file.name) ? 'input' : 'output';
-            let testCase = allTests.find(c => c.number == number);
+            let testCase = allTests.find(test => test[0] == number);
 
             if (testCase == undefined) {
-                testCase = { number: number }
-
-                if (type == 'input') {
-                    testCase.input = true;
-                } else {
-                    testCase.output = true;
-                }
+                testCase = [number, { input: false, output: false }];
 
                 allTests.push(testCase);
-            } else {
-                if (type == 'input') {
-                    testCase.input = true;
-                } else {
-                    testCase.output = true;
-                }
             }
+
+            if (type == 'input') testCase[1].input = true;
+
+            if (type == 'output') testCase[1].output = true;
 
             return allTests;
         }, [])
-        .filter(testCase => testCase.input && testCase.output)
-        .map(testCase => testCase.number);
+        .filter(testCase => testCase[1].output);
 }
 
-async function executeScript(path, scenarioNumber) {
-    const script = spawn('node', [`${path}/index.js`]);
-    
-    const input = readFileSync(`${path}/input_${scenarioNumber}.txt`, 'UTF-8');
+async function executeScript(path, scenarioNumber, options) {
+    const script = spawn('node', [`${path}/main.js`]);
 
-    script.stdin.write(input);
-    script.stdin.end();
+    if (options.input) {
+        const input = readFileSync(`${path}/input_${scenarioNumber}.txt`, 'UTF-8');
+
+        script.stdin.write(input);
+        script.stdin.end();
+    }
 
     let output = "";
     let error = "";
