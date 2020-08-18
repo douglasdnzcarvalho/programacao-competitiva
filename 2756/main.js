4ee52e5138448e415f1533708cf9ed2dbb670328
Douglas Diniz Carvalho <douglasdnzcarvalho@gmail.com>
2020-08-18 01:12:44 -0300

    Resolução do teste 2756

---
+++ 2756/main.js
@@ -0,0 +1,13 @@
+let output = [];
+
+output.push('       A');
+output.push('      B B');
+output.push('     C   C');
+output.push('    D     D');
+output.push('   E       E');
+output.push('    D     D');
+output.push('     C   C');
+output.push('      B B');
+output.push('       A');
+
+console.log(output.join('\n'));
